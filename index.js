fdcde67fdcad9793628f2609e40319797d3f57fb
HannaTverdokhleb <1annakiss92@gmail.com>
2024-03-23 12:39:03 +0400

    optimize

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-const swiper = new Swiper('.swiper', {
-  speed: 400,
-  slidesPerView: 1,
-  spaceBetween: 10,
-  loop: true,   
-  autoplay: {
-    delay: 4000,
-  },
-  navigation: {
-    nextEl: '.arrow_right',
-    prevEl: '.arrow_left',
-  },
-  breakpoints: {
-    420: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 40
-    },
-    992: {
-      slidesPerView: 4,
-    },
-  }
-});
-
 (function() {
   var lastScrollTop = document.documentElement.scrollTop;
   let header = document.querySelector('.header');
@@ -61,5 +34,32 @@ const swiper = new Swiper('.swiper', {
       }
       lastScrollTop = st <= 0 ? 0 : st;
    }, false);
+
+   const swiper = new Swiper('.swiper', {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+    },
+    navigation: {
+      nextEl: '.arrow_right',
+      prevEl: '.arrow_left',
+    },
+    breakpoints: {
+      420: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      },
+      992: {
+        slidesPerView: 4,
+      },
+    }
+  });
 })();
 
